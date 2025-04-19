712e12f5840abf510c16ad45e3e698e4d08bb6ee
philluple <pnl2111@columbia.edu>
2025-04-18 21:49:19 -0400

    oops

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBW_puorIgVolquZs3ZHRtFROARv9okkw8",
-  authDomain: "hci-testbe.firebaseapp.com",
-  projectId: "hci-testbe",
-  storageBucket: "hci-testbe.firebasestorage.app",
-  messagingSenderId: "1012812271449",
-  appId: "1:1012812271449:web:9621620d81915fe1684648",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
